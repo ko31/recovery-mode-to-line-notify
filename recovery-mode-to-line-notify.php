96b3a1b28eec47980ddb6ff9ce035716c00ba3c0
Ko Takagi <knockoutmarch@gmail.com>
2019-05-10 17:36:56 +0900

    :wrench: Create scaffold

---
+++ recovery-mode-to-line-notify.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Recovery Mode To Line Notify
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          ko31
+ * Author URI:      https://go-sign.info
+ * Text Domain:     recovery-mode-to-line-notify
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Recovery_Mode_To_Line_Notify
+ */
+
+// Your code starts here.
